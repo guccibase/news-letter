273fc0d5382f73b9a60e20793c343e63b5f03d2a
guccibase <tahiru@tryhungry.com>
2020-06-02 22:50:20 -0400

    heroku added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const request = require("request");
+const http = require("https");
 
 const app = express();
 app.use(express.static("public"))
@@ -16,11 +17,73 @@ app.get("/", (reg, res)=>{
 
 
 app.post("/", (reg, res)=>{
-    var firstName = reg.body.first;
-    var lastName = reg.body.last;
-    var email = reg.body.email;
+    const firstName = reg.body.first;
+    const lastName = reg.body.last;
+    const email = reg.body.email;
 
     console.log(firstName, lastName, email);
+
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+
+            }
+        ]
+    }
+
+
+    const url = "https://us10.api.mailchimp.com/3.0/lists/4215854fac"
+    const jsonData = JSON.stringify(data)
+    const options = {
+        method: "POST",
+        auth: "guccibase:0a7444aa601b039643b3e50638d7a364"
+    }
+
+    const request = http.request(url, options, (response)=>{
+
+        response.on("data", (data)=>{
+            console.log(JSON.parse(data));
+            
+        })
+
+        if(response.statusCode === 20){
+            res.sendFile(__dirname + "/success.html")
+
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+
+        }
+
+
+    })
+
+    request.write(jsonData)
+
+
+    
+
+    request.end()
+
     
 
 })
+
+
+app.post("/failure", (reg, res)=>{
+
+    res.redirect("/")
+
+})
+
+//"https://us10.api.mailchimp.com/3.0/lists/4215854fac"
+
+//0a7444aa601b039643b3e50638d7a364-us10
+
+//4215854fac
